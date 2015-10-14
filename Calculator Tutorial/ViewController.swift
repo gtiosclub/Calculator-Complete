4dd9c77cf0c8cfc96d35f1828a142d2b9bb1ebf8
Cal Stephens <calstephens98@gmail.com>
2015-10-14 18:42:09 -0400

    Initial Commit

---
+++ Calculator Tutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator Tutorial
+//
+//  Created by Cal on 10/14/15.
+//  Copyright © 2015 Cal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
